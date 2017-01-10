795960a4457ae9ae89f6e5686acc188e690453d5
askello <oleksii.korpan@gmail.com>
2017-01-10 12:49:32 +0200

    fixed first, last, random conditions. Added isRawSql method. Now raw sql available for sortBy method

--- upload/system/library/db/QueryBuilder/Common/Conditions.php
+++ upload/system/library/db/QueryBuilder/Common/Conditions.php
@@ -2,29 +2,29 @@
 namespace db\QueryBuilder\Common;
 
 trait Conditions {
-	
+
 	private $conditions_sql = '';
-	
+
 	public function where($conditions) {
 		$args = func_get_args();
-		
+
 		$sql = $this->createWhereSql($args);
-		
+
 		$this->appendCondition($sql);
-		
+
 		return $this;
 	}
-	
+
 	public function orWhere($conditions) {
 		$args = func_get_args();
-		
+
 		$sql = $this->createWhereSql($args);
-		
+
 		$this->appendCondition($sql, "OR");
-		
+
 		return $this;
 	}
-	
+
 	private function createWhereSql($args) {
 		if(count($args) > 1) {
 			$sql = $this->parseSingleCondition($args[0], $args[1]);
@@ -33,69 +33,77 @@ trait Conditions {
 		} else {
 			$sql = $args[0];
 		}
-		
+
 		return $sql;
 	}
-	
+
 	public function find($keys) {
 		if(!is_array($keys)) {
 			$this->limit(1);
 		}
-		
+
 		if(is_int($keys) or is_string($keys)) {
 			$this->where($this->getPrimaryKey(), $keys);
 		} else {
 			$this->whereIn($this->getPrimaryKey(), $keys);
 		}
-		
+
 		return $this;
 	}
-	
+
 	public function first($limit = 1) {
 		$this->limit($limit);
-		
+
+		if($this->_order() == "") {
+			$this->sortBy($this->getPrimaryKey());
+		}
+
 		return $this;
 	}
-	
+
 	public function last($limit = 1) {
 		$this->limit($limit);
-		
-		$this->sortOrder = $this->sortOrder == "DESC" ? "ASC" : "DESC";
-		
+
+		if($this->_order() == "") {
+			$this->sortBy($this->getPrimaryKey(), "DESC");
+		} else {
+			$this->sortViceVersa();
+		}
+
 		return $this;
 	}
-	
+
 	public function random($limit = 1) {
 		$this->limit($limit);
-		
-		$this->sortField = "RAND()";
-		
+
+		$this->sortBy("RAND()");
+
 		return $this;
 	}
-	
+
 	private function parseSingleCondition($field, $value) {
 		$field = trim($field);
-		
+
 		$operator = $this->determineOperator($field, $value);
 		$field = $this->getConditionField($field);
 		$value = $this->parseConditionValue($value, $operator);
-		
+
 		return $field." ".$operator." ".$value;
 	}
-	
+
 	private function getConditionField($field) {
 		$data = explode(' ', $field, 2);
 		return $this->_field($data[0]);
 	}
-	
+
 	private function parseConditionValue($value) {
 		if(is_int($value) or is_float($value)) {
 			return $value;
 		}
-		
+
 		if(is_array($value)) {
 			$values = array();
-			
+
 			foreach($value as $val) {
 				if(is_int($val) or is_float($val)) {
 					$values[] = $val;
@@ -103,25 +111,25 @@ trait Conditions {
 					$values[] = "'".$this->escape($val)."'";
 				}
 			}
-			
+
 			return "(".implode(",", $values).")";
 		}
-		
+
 		if(is_null($value)) {
 			return "NULL";
 		}
-		
+
 		return "'".$this->escape($value)."'";
 	}
-	
+
 	private function determineOperator($field, $value) {
 		$operator = "=";
-		
+
 		if(strpos($field, ' ') !== false) {
 			$data = explode(' ', $field, 2);
 			$operator = trim($data[1]);
 		}
-		
+
 		if(is_array($value)) {
 			if($operator == "!=") {
 				return "NOT IN";
@@ -129,7 +137,7 @@ trait Conditions {
 				return "IN";
 			}
 		}
-		
+
 		if(is_null($value)) {
 			if($operator == "!=") {
 				return "IS NOT";
@@ -137,39 +145,39 @@ trait Conditions {
 				return "IS";
 			}
 		}
-		
+
 		return $operator;
 	}
-	
+
 	private function parseConditions($conditions) {
 		$sql = "";
-		
+
 		$delimiter = "";
-		
-		foreach($conditions as $field => $value) {				
+
+		foreach($conditions as $field => $value) {
 			if(is_int($field)) {
 				if(trim(strtoupper($value)) == "OR") {
 					$delimiter = " OR ";
 					continue;
 				}
-				
+
 				if(is_string($value)) {
 					$sql .= $delimiter.$value;
 				}
 			} else {
 				$sql .= $delimiter.$this->parseSingleCondition($field, $value);
 			}
-			
+
 			$delimiter = " AND ";
 		}
-		
+
 		if(count($conditions > 1)) {
 			$sql = "(".$sql.")";
 		}
-		
+
 		return $sql;
 	}
-	
+
 	private function appendCondition($condition, $operator = "AND") {
 		if(!$this->conditions_sql and $condition) {
 			$this->conditions_sql = PHP_EOL."WHERE ".$condition;
@@ -177,9 +185,9 @@ trait Conditions {
 			$this->conditions_sql .= " ".$operator." ".$condition;
 		}
 	}
-	
+
 	private function _where() {
 		return $this->conditions_sql;
 	}
-	
+
 }
--- upload/system/library/db/QueryBuilder/Common/Order.php
+++ upload/system/library/db/QueryBuilder/Common/Order.php
@@ -2,43 +2,60 @@
 namespace db\QueryBuilder\Common;
 
 trait Order {
-	
+
 	private $sortFields = array();
-	
+
 	public function sortBy($field, $order = "ASC") {
 		if(is_array($field)) {
 			foreach($field as $field_name => $order) {
 				$this->sortBy($field_name, $order);
-			}			
+			}
 		} else {
-			$this->sortFields[$field] = $order;
+			if($this->isRawSql($field)) {
+				$this->sortFields[] = $field;
+			} else {
+				$this->sortFields[$field] = $order;
+			}
 		}
-		
+
 		return $this;
 	}
-	
+
 	private function sortOrder($order) {
 		$order = strtoupper($order);
-		
+
 		if($order != "DESC") {
 			$order = "ASC";
 		}
-		
+
 		return $order;
 	}
-	
+
+	private function sortViceVersa() {
+		foreach(array_keys($this->sortFields) as $sortField) {
+			if(!is_int($sortField)) {
+				$sortOrder = $this->sortOrder($this->sortFields[$sortField]) == "ASC" ? "DESC" : "ASC";
+				$this->sortBy($sortField, $sortOrder);
+			}
+		}
+	}
+
 	private function _order() {
 		if($this->sortFields) {
 			$fields = array();
-			
+
 			foreach($this->sortFields as $field => $order) {
-				$fields[] = $this->_field($field)." ".$this->sortOrder($order);
+				if(is_int($field)) { // if raw sql
+					$fields[] = $order;
+				} else {
+					$fields[] = $this->_field($field)." ".$this->sortOrder($order);
+				}
 			}
-			
+
 			return PHP_EOL."ORDER BY ".implode(", ", $fields);
 		}
-		
+
 		return "";
 	}
-	
+
 }
